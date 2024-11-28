f90e2b3ca4122fe642c68854f46beae22ab6c9b4
beer-psi <lacvtg.a1.2023@gmail.com>
2024-11-28 21:49:12 +0700

    fix: sort twitter timeline

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 ## facebook-feed-to-rss
+
 RSSify your Facebook feed, so you can use it with readers or RSS bots on chat platforms.
 
 ### Obtaining a Facebook Graph API token
+
 1. [Link your account with Instagram.](https://www.facebook.com/dialog/oauth?client_id=124024574287414&redirect_uri=fbconnect%3A%2F%2Fsuccess&scope=email%2Cpublish_actions%2Cpublish_pages%2Cuser_about_me%2Cuser_actions.books%2Cuser_actions.music%2Cuser_actions.news%2Cuser_actions.video%2Cuser_activities%2Cuser_birthday%2Cuser_education_history%2Cuser_events%2Cuser_games_activity%2Cuser_groups%2Cuser_hometown%2Cuser_interests%2Cuser_likes%2Cuser_location%2Cuser_notes%2Cuser_photos%2Cuser_questions%2Cuser_relationship_details%2Cuser_relationships%2Cuser_religion_politics%2Cuser_status%2Cuser_subscriptions%2Cuser_videos%2Cuser_website%2Cuser_work_history%2Cfriends_about_me%2Cfriends_actions.books%2Cfriends_actions.music%2Cfriends_actions.news%2Cfriends_actions.video%2Cfriends_activities%2Cfriends_birthday%2Cfriends_education_history%2Cfriends_events%2Cfriends_games_activity%2Cfriends_groups%2Cfriends_hometown%2Cfriends_interests%2Cfriends_likes%2Cfriends_location%2Cfriends_notes%2Cfriends_photos%2Cfriends_questions%2Cfriends_relationship_details%2Cfriends_relationships%2Cfriends_religion_politics%2Cfriends_status%2Cfriends_subscriptions%2Cfriends_videos%2Cfriends_website%2Cfriends_work_history%2Cads_management%2Ccreate_event%2Ccreate_note%2Cexport_stream%2Cfriends_online_presence%2Cmanage_friendlists%2Cmanage_notifications%2Cmanage_pages%2Cphoto_upload%2Cpublish_stream%2Cread_friendlists%2Cread_insights%2Cread_mailbox%2Cread_page_mailboxes%2Cread_requests%2Cread_stream%2Crsvp_event%2Cshare_item%2Csms%2Cstatus_update%2Cuser_online_presence%2Cvideo_upload%2Cxmpp_login&response_type=token)
 2. Copy and paste this URL:
+
 ```
 view-source:https://www.facebook.com/dialog/oauth?client_id=124024574287414&redirect_uri=https://www.instagram.com/accounts/signup/&&scope=email&response_type=token
 ```
+
 3. Copy the access token from the address bar.
--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 import * as dotenv from "@std/dotenv";
 import * as cheerio from "cheerio";
 import { Hono } from "@hono/hono";
+import { getConnInfo } from "@hono/hono/deno";
 import { HTTPException } from "@hono/hono/http-exception";
-import { logger } from "@hono/hono/logger";
+import { requestId, RequestIdVariables } from "@hono/hono/request-id";
+import { getPath, getQueryParams } from "@hono/hono/utils/url";
 import { openKvToolbox } from "@kitsonk/kv-toolbox";
 import { Feed } from "feed";
 import makeFetchCookie from "fetch-cookie";
@@ -47,9 +49,41 @@ if (TWITTER_COOKIES) {
     parseMozillaCookies(jar, TWITTER_COOKIES);
 }
 
-const app = new Hono();
+const app = new Hono<{
+    Variables: RequestIdVariables;
+}>();
 
-app.use(logger());
+app.use(requestId());
+app.use(async (c, next) => {
+    const connInfo = getConnInfo(c);
+    const method = c.req.method;
+    const path = getPath(c.req.raw);
+    const params = getQueryParams(c.req.raw.url);
+    const time = new Date();
+
+    console.log(
+        `<-- id=${
+            c.get("requestId")
+        } ip=${connInfo.remote.address} method=${method} path=${path} query=${
+            JSON.stringify(params ?? {})
+        } timestamp=${time.toISOString()}`,
+    );
+
+    await next();
+
+    const status = c.res.status;
+    let logFn = console.log;
+
+    if (status >= 500) {
+        logFn = console.error;
+    } else if (status >= 400) {
+        logFn = console.warn;
+    }
+
+    logFn(
+        `--> id=${c.get("requestId")} status=${c.res.status} time=${Date.now() - time.valueOf()}ms`,
+    );
+});
 
 app.get("/facebook/image/:id", async (c) => {
     const url = new URL(
@@ -286,7 +320,7 @@ app.get("/twitter-rss/:username", async (c) => {
         const items = await Array.fromAsync(
             kv.list<Item>(
                 { prefix: ["twitter", username] },
-                { consistency: "eventual" }
+                { consistency: "eventual" },
             ),
         );
 
@@ -321,6 +355,11 @@ app.get("/twitter-rss/:username", async (c) => {
     if (timeline.entries.length < 1) {
         return c.text("");
     }
+
+    timeline.entries.sort((a, b) =>
+        new Date(b.content.tweet.created_at).valueOf() -
+        new Date(a.content.tweet.created_at).valueOf()
+    );
 
     const user = timeline.entries[0].content.tweet.user;
     const feedOptions: FeedOptions = {

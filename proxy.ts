9b0aee22dc77b15d06850d9933cd13209e4b00bd
beer-psi <lacvtg.a1.2023@gmail.com>
2024-12-04 14:22:29 +0700

    proxy container

---
+++ proxy.ts
@@ -0,0 +1,36 @@
+import { Hono } from "@hono/hono";
+import { basicAuth } from "@hono/hono/basic-auth";
+
+const AUTH_USER = Deno.env.get("AUTH_USER");
+const AUTH_PASSWORD = Deno.env.get("AUTH_PASSWORD");
+const PORT = Deno.env.get("PORT");
+
+const app = new Hono();
+
+if (AUTH_USER || AUTH_PASSWORD) {
+    app.use(
+        basicAuth({
+            username: AUTH_USER ?? "",
+            password: AUTH_PASSWORD ?? "",
+        }),
+    );
+}
+
+app.on(["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"], "/*", async (c) => {
+    const url = c.req.path.slice(1);
+    
+    const init: RequestInit = {
+        credentials: "include",
+        headers: c.req.header(),
+        method: c.req.method,
+        redirect: "manual",
+    };
+
+    if (!["GET", "HEAD"].includes(c.req.method)) {
+        init.body = await c.req.blob();
+    }
+
+    return fetch(url, init);
+});
+
+Deno.serve({ port: PORT ? Number(PORT) : 8888 }, app.fetch);
